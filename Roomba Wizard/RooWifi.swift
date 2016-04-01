317bcb86bb09c042b851bcc435ddb6fd0facc7ce
Brett Granborg <Granborg@Ori.local>
2016-04-01 14:03:48 -0700

    first commit

---
+++ Roomba Wizard/RooWifi.swift
@@ -0,0 +1,13 @@
+//
+//  RooWifi.swift
+//  Roomba Wizard
+//
+//  Created by Brett Granborg on 4/1/16.
+//  Copyright © 2016 Brett Granborg. All rights reserved.
+//
+
+import UIKit
+
+class RooWifi: NSObject {
+
+}
